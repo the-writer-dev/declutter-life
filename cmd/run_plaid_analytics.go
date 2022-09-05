cda5d84a732f31609a6421ec0d6dbdb44a71005f
Joonhyeok Ahn <joonhyeok.ahn3@wework.com>
2022-09-05 16:36:24 -0500

    Add cobra command

---
+++ cmd/run_plaid_analytics.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var runPlaidAnalytics = &cobra.Command{
+	Use:   "run_plaid_analytics",
+	Short: "Run Plaid Analytics in background",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/bitethecode/declutter-life/cmd"
+
+func main() {
+	cmd.Execute()
+}
